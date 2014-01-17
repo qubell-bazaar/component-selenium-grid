5d271dd3b60ad3bb2efb457a7b333bfbc097205a
Dmytro Makhno <makh.dv@gmail.com>
2014-01-17 15:49:39 +0200

    adding test

---
+++ test/test_remote_webdriver.py
@@ -0,0 +1,34 @@
+import os
+
+from test_runner import BaseComponentTestCase
+from qubell.api.private.testing import instance, workflow, values
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+class ComponentTestCase(BaseComponentTestCase):
+    name = "component-selenium-grid"
+    apps = [{
+        "name": name,
+        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name))
+    }]
+
+    def console_base(capability, console, remote):
+        try:
+            driver = webdriver.Remote(
+                command_executor=remote,
+                desired_capabilities=capability)
+            driver.get(console)
+            assert driver.title == "Grid Console"
+        finally:
+            driver.quit()
+
+
+    @instance(byApplication=name)
+    @values({"endpoints.console-url": "console", "endpoints.remote-url": "remote"})
+    def test_chrome(self, instance, console, remote):
+        console_base(DesiredCapabilities.CHROME, console, remote)
+
+    @instance(byApplication=name)
+    @values({"endpoints.console-url": "console", "endpoints.remote-url": "remote"})
+    def test_chrome(self, instance, console, remote):
+        console_base(DesiredCapabilities.FIREFOX, console, remote)
