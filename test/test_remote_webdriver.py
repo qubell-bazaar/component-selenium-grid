c33788f2da67553e5dde2be8ba2c17afd9b85ba1
Anton Butovich <abutovich@gmail.com>
2015-09-30 15:48:21 +0300

    Addes windows support for hub and node

--- test/test_remote_webdriver.py
+++ test/test_remote_webdriver.py
@@ -9,47 +9,65 @@ from qubell.api.testing import *
 
 # noinspection PyShadowingNames,PyUnusedLocal
 @environment({
-    "chef_11": {},  # default
-    "chef_12": {
-        "policies": [{
-                    "action": "chefrun",
-                    "parameter": "version",
-                    "value": "12.2.1"
-        }]
-    }
-
+    "default": {}
 })
+
+@applications([{
+  "name": 'Linux selenium-hub',
+  "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name)),
+  "parameters": { "input.hub-image": { "ami": "us-east-1/ami-96a818fe", "user": "centos", "type": "linux", "hw": "m3.medium", "issudo": true, "prefix": "Selenium-hub" }}
+  },
+  {
+  "name": 'Windows selenium-hub',
+  "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name)),
+  "parameters": { "input.hub-image": { "ami": "us-east-1/ami-31620c54", "user": "Administrator", "type": "windows", "hw": "m3.large", "issudo": false, "prefix": "Selenium-hub" }}
+}])
+
 class SeleniumGridComponentTestCase(BaseComponentTestCase):
-    name = "component-selenium-grid"
-    apps = [{
-            "name": name,
-            "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name))
-            }]
 
-    def console_base(self, capability, console, remote):
-        time.sleep(15)  # let node establish connection with hub
-        driver = None
-        try:
-            driver = webdriver.Remote(
-                command_executor=str(remote),
-                desired_capabilities=capability)
-            driver.get(console)
-            assert driver.title == "Grid Console"
-        finally:
-            if driver:
-                driver.quit()
-
-    @instance(byApplication=name)
-    @values({"endpoints.console-url": "console", "endpoints.remote-url": "remote"})
-    def test_chrome(self, instance, console, remote):
+  def console_base(self, instance, capability):
+    console = instance.returnValues['endpoints.console-url']
+    remote = instance.returnValues['endpoints.remote-url"']
+    time.sleep(15)  # let node establish connection with hub
+    driver = None
+    try:
+      driver = webdriver.Remote(
+        command_executor=str(remote),
+        desired_capabilities=capability)
+      driver.get(console)
+      assert driver.title == "Grid Console"
+    finally:
+      if driver:
+        driver.quit()
+  
+  @instance(byApplication='Linux selenium-hub')
+  def test_chrome(self, instance):
         capabilities = webdriver.ChromeOptions().to_capabilities()
         # https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
         # Passing '--no-sandbox' flag when creating your WebDriver session.
         # Special test environments sometimes cause Chrome to crash when the sandbox is enabled.
         capabilities['chromeOptions']['args'].append('--no-sandbox')
-        self.console_base(capabilities, console, remote)
+        self.console_base(instance, capabilities, console, remote)
 
-    @instance(byApplication=name)
-    @values({"endpoints.console-url": "console", "endpoints.remote-url": "remote"})
-    def test_firefox(self, instance, console, remote):
+  @instance(byApplication='Linux selenium-hub')
+  def test_firefox(self, instance):
         self.console_base(DesiredCapabilities.FIREFOX, console, remote)
+
+
+  @instance(byApplication='Windows selenium-hub')
+  def test_chrome(self, instance):
+        capabilities = webdriver.ChromeOptions().to_capabilities()
+        # https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
+        # Passing '--no-sandbox' flag when creating your WebDriver session.
+        # Special test environments sometimes cause Chrome to crash when the sandbox is enabled.
+        capabilities['chromeOptions']['args'].append('--no-sandbox')
+        self.console_base(instance, capabilities, console, remote)
+  
+  
+  @instance(byApplication='Windows selenium-hub')
+  def test_firefox(self, instance):
+        self.console_base(DesiredCapabilities.FIREFOX, console, remote)
+  
+  @instance(byApplication='Linux selenium-hub')
+  def test_ie(self, instance):
+        self.console_base(DesiredCapabilities.INTERNETEXPLORER, console, remote)
