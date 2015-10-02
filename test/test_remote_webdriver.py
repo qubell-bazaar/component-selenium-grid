889a83d78edd3a7e234343a50ed2b95eb7defd21
Anton Butovich <abutovich@gmail.com>
2015-10-02 16:39:07 +0300

    moved component-skeleton to version 3.3

--- test/test_remote_webdriver.py
+++ test/test_remote_webdriver.py
@@ -3,71 +3,78 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import WebDriverException
 
 from qubell.api.testing import *
+from qubell.api.tools import retry
 
+manifest_name = "component-selenium-grid.yml"
 
 # noinspection PyShadowingNames,PyUnusedLocal
-@environment({
-    "default": {}
-})
-
-@applications([{
-  "name": 'Linux selenium-hub',
-  "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name)),
-  "parameters": { "input.hub-image": { "ami": "us-east-1/ami-96a818fe", "user": "centos", "type": "linux", "hw": "m3.medium", "issudo": true, "prefix": "Selenium-hub" }}
-  },
-  {
-  "name": 'Windows selenium-hub',
-  "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name)),
-  "parameters": { "input.hub-image": { "ami": "us-east-1/ami-31620c54", "user": "Administrator", "type": "windows", "hw": "m3.large", "issudo": false, "prefix": "Selenium-hub" }}
-}])
-
 class SeleniumGridComponentTestCase(BaseComponentTestCase):
+  @classmethod
+  def timeout(cls):
+      return 60 
 
   def console_base(self, instance, capability):
     console = instance.returnValues['endpoints.console-url']
-    remote = instance.returnValues['endpoints.remote-url"']
-    time.sleep(15)  # let node establish connection with hub
-    driver = None
-    try:
-      driver = webdriver.Remote(
+    remote = instance.returnValues['endpoints.remote-url']
+
+    @retry(30,4,1, Exception)
+    def get_driver():
+      return webdriver.Remote(
         command_executor=str(remote),
         desired_capabilities=capability)
+
+    driver = None
+    try:
+      driver = get_driver()
       driver.get(console)
       assert driver.title == "Grid Console"
     finally:
-      if driver:
+      try:
         driver.quit()
-  
+      except:
+        pass
+
+@applications([{
+  "name": 'Linux selenium-hub',
+  "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '..', manifest_name)),
+  "parameters": { "input.hub-image": { "ami": "us-east-1/ami-96a818fe", "user": "centos", "type": "linux", "hw": "m3.medium", "issudo": True, "prefix": "Selenium-hub" }}
+}])
+class LinuxGrid(SeleniumGridComponentTestCase):
   @instance(byApplication='Linux selenium-hub')
-  def test_chrome(self, instance):
+  def test_lin_chrome(self, instance):
         capabilities = webdriver.ChromeOptions().to_capabilities()
         # https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
         # Passing '--no-sandbox' flag when creating your WebDriver session.
         # Special test environments sometimes cause Chrome to crash when the sandbox is enabled.
         capabilities['chromeOptions']['args'].append('--no-sandbox')
-        self.console_base(instance, capabilities, console, remote)
+        self.console_base(instance, capabilities)
 
   @instance(byApplication='Linux selenium-hub')
-  def test_firefox(self, instance):
-        self.console_base(DesiredCapabilities.FIREFOX, console, remote)
+  def test_lin_firefox(self, instance):
+        self.console_base(instance, DesiredCapabilities.FIREFOX)
 
-
+@applications([{
+  "name": 'Windows selenium-hub',
+  "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '..', manifest_name )),
+  "parameters": { "input.hub-image": { "ami": "us-east-1/ami-31620c54", "user": "Administrator", "type": "windows", "hw": "m3.large", "issudo": False, "prefix": "Selenium-hub" }}
+}])
+class WindowsGrid(SeleniumGridComponentTestCase):
   @instance(byApplication='Windows selenium-hub')
-  def test_chrome(self, instance):
+  def test_win_chrome(self, instance):
         capabilities = webdriver.ChromeOptions().to_capabilities()
         # https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
         # Passing '--no-sandbox' flag when creating your WebDriver session.
         # Special test environments sometimes cause Chrome to crash when the sandbox is enabled.
         capabilities['chromeOptions']['args'].append('--no-sandbox')
-        self.console_base(instance, capabilities, console, remote)
-  
+        self.console_base(instance, capabilities)
   
   @instance(byApplication='Windows selenium-hub')
-  def test_firefox(self, instance):
-        self.console_base(DesiredCapabilities.FIREFOX, console, remote)
+  def test_win_firefox(self, instance):
+        self.console_base(instance, DesiredCapabilities.FIREFOX)
   
-  @instance(byApplication='Linux selenium-hub')
-  def test_ie(self, instance):
-        self.console_base(DesiredCapabilities.INTERNETEXPLORER, console, remote)
+  @instance(byApplication='Windows selenium-hub')
+  def test_win_ie(self, instance):
+        self.console_base(instance, DesiredCapabilities.INTERNETEXPLORER)
